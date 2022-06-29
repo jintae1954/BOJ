c226aded7d39b674bec809f985492cf3bf59b2ab
JINTAE <qkrwlsxo1954@gmail.com>
2022-06-29 16:08:03 +0900

    boj_4999, boj_9086,

--- Day_31.py
+++ Day_31.py
@@ -1,3 +1,4 @@
+# boj 4999
 patient = list(input().split('h'))
 doctor = list(input().split('h'))
 
@@ -8,3 +9,13 @@ if len(patient[0]) >= len(doctor[0]):
     print('go')
 else:
     print('no')
+    
+    
+# boj 9086    
+cases = int(input())
+case = 0
+
+while case < cases:
+    case += 1
+    string = input()
+    print('{}{}'.format(string[0], string[-1]))
